9a8ae6ccf061274859f6e135635eb5743878fac9
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-17 16:04:59 +0500

    feat: добавить краш-тест

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,12 @@ app.use(
 
 app.use(requestLogger); // подключаем логгер запросов
 
+app.get('/crash-test', () => { // после прохождения ревью код краш-теста необходимо удалить
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', express.json(), validationsLogin, login);
 app.post('/signup', express.json(), validationsCreateUser, createUser);
 
