c72bd4c1ec7e3f57e3975ce44da7f8f5ed965984
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-18 18:46:54 +0500

    fix: исправить ошибку при передаче секретного ключа

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -19,7 +19,7 @@ const isAuthtorized = async (req, res, next) => {
   let payload;
 
   try {
-    payload = await jwt.verify(token, JWT_SECRET);
+    payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new AuthorizationError('Необходима авторизация')); // 401
     return;
